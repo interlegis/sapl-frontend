0efc186b2caa019701d972b20527b3d3239110ad
Leandro Roberto <leandroroberto.br@gmail.com>
2019-01-17 00:47:33 -0200

    add tema jquery-ui, tema cupertino e fontawesomme-free

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
+
+import 'jquery-ui-themes/themes/cupertino/jquery-ui.min.css'
+
 import "./scss/app.scss";
+
 import "bootstrap";
+
+
+import 'webpack-jquery-ui/datepicker'
+import 'jquery-ui/ui/i18n/datepicker-pt-BR'
+
+window.refreshDatePicker = function() {
+  $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+  $('.dateinput').datepicker();
+}
+
+window.refreshDatePicker();
