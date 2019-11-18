259b2f95ed6c5117c594781a94517498af34a56c
ulyssesBML <ulysses3353@gmail.com>
2019-11-18 13:03:10 -0300

    Adaptando pagina de parlamentar para vuejs Fix #3030

--- src/__apps/parlamentar/main.js
+++ src/__apps/parlamentar/main.js
@@ -2,7 +2,10 @@ import './scss/parlamentar.scss'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { FormSelectPlugin } from 'bootstrap-vue'
+import axios from 'axios'
 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 Vue.use(FormSelectPlugin)
 
@@ -13,25 +16,86 @@ new Vue({
     el: '#app2',
     data() {
         return{
+          nome_pesquisa: "",
+          is_pesquisa: false,
+          legislatura_selecionada:"",
           legislaturas: [],
-          options: [
-            {text:"asd",value:1},
-          ],
-          legislatura_selecionada:"asdasd"
+          parlamentares: [],
         }
+        
+    },
+    
+    watch: {
+      nome_pesquisa : function (val) {
+        this.debouncepesquisaParlamentar()
+      }
+    },
+
+    created() {
+      this.debouncepesquisaParlamentar = _.debounce(this.pesquisaParlamentar, 200)
+    },
+
+    methods: {
+      getParlamentares(event) {
+        console.log("asd")
+        if (this.legislatura_selecionada){
+          axios.get('/parlamentar/get_parlamentare_by_legislaturas_json/'+this.legislatura_selecionada)
+          .then(response => {
+            console.log(response.data['parlamentares'][0])
+            this.parlamentares = response.data['parlamentares']
+          })
+          .catch(err => {
+            // Do something for an error here
+            console.log(err)
+            console.error("Ocorreu um erro ao pegar os dados de parlamentares")
+          })
+        }
+      },
+
+      pesquisaParlamentar(event){
+        var bodyFormData = new FormData();
+        bodyFormData.set('nome', this.nome_pesquisa);
+        
+        axios({
+          method: 'post',
+          url: '/parlamentar/search_parlamentare_json/',
+          data: bodyFormData,
+          config: { headers: {'Content-Type': 'multipart/form-data' }}
+          })
+          .then((response) => {
+              this.parlamentares = response.data['parlamentares']
+          })
+          .catch(function (response) {
+              console.log(response);
+          });
+      },
+
+      pesquisaChange(event){
+        this.is_pesquisa=!this.is_pesquisa;
+        if (this.is_pesquisa){
+          this.parlamentares = []
+        }
+        else{
+          this.getParlamentares()    
+        }
+      }
+
     },
 
     mounted (){
-      fetch('/parlamentar/get-all-legislaturas_json/')
+      axios.get('/parlamentar/get-all-legislaturas_json/')
         .then(response => {
-          return response.json()
+          console.log(response)
+          this.legislaturas = response.data.legislaturas
+          this.legislatura_selecionada =  response.data.legislaturas[0][0]
         })
-        .then(data => {
-           // Work with JSON data here
-           this.legislaturas = data.legislaturas
+        .then(response => {
+          this.getParlamentares()
         })
         .catch(err => {
           // Do something for an error here
+          console.log(err)
+          console.error("Ocorreu um erro ao pegar os dados de legislação")
         })
     }
   }) 
